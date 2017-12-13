aa2105d9cc6bcc72fbe31ce96369f57e69386fc7
Phillip Doehle <doehle@okstate.edu>
2017-12-13 14:26:38 -0600

    Put table of contents in navigation bar

--- make_toc.sh
+++ make_toc.sh
@@ -3,17 +3,13 @@
 # Name: make_toc.sh
 # Usage: make_toc.sh [Markdown file]
 # Description: Creates a toc from a Markdown file.
-#              The end result is a nested list with
+#              The end result is a list with
 #              each heading in the form of a Markdown
 #              anchor link.
 
 # Temp files
 # HEADERS: contains headers filtered out from the text
-# LEVELS: contains nesting information to make the toc in the form of a list
-# LINKS: contains the headers reformated as links (using Markdown syntax)
 HEADERS=$(mktemp)
-MARKERS=$(mktemp)
-LINKS=$(mktemp)
 
 # Pull out section headings to create toc
 # The Markdown headings start with `#`
@@ -24,25 +20,22 @@ LINKS=$(mktemp)
 cat "$1" | sed 's/#!//g' | sed 's/#PBS//g' | sed 's/#\ \ //g' | grep "^#\ " > $HEADERS 
 
 # Remove heading markers for easier processing
-# Put in separate file for safe keeping
 sed -i 's/^#*\ //g' $HEADERS 
-while read LINE
-do
-	echo "-" >> $MARKERS
-done < $HEADERS
+
+# Entries are appended to toc file
+if [ -e "toc.md" ]
+then
+	rm -f toc.md
+fi
 # Create anchor links out of each header using Markdown
 # Links are written to the final toc file
 while read LINE 
 do
 	NOSPACE=$(echo $LINE | sed 's/\ /-/g' | tr '[:upper:]' '[:lower:]')
-	echo "[$LINE](#$NOSPACE)" >> $LINKS
+	echo "[$LINE](#$NOSPACE)" >> toc.md
+	echo -e "\n" >> toc.md
 done < $HEADERS
-# Link are put in Markdown list
-if [ -e "toc.md" ]
-then
-	rm -f toc.md
-fi
-paste -d' ' $MARKERS $LINKS >> toc.md
 
-# Add a title
-sed -i '1s/^/# Table of Contents\n/' toc.md
+# Add html <div> so that it integrates with css sheet
+sed -i '1s/^/\<div class=\"sidenav\"\>/' toc.md
+sed -ie "\$a\<\/div\>" toc.md
